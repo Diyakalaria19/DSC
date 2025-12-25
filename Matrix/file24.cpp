930eecd152213e7b021d9c40fd930d1b15aaade8
Diya K <diyakalaria19@gmail.com>
2025-12-25 16:20:02 +0530

    Add files via upload

---
+++ Matrix/file24.cpp
@@ -0,0 +1,38 @@
+//Find row with maximum no. of 1's
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int rowWithMax1s(vector<vector<bool>>& mat) {
+    int maxRow = -1, row = 0;
+    int R = mat.size();
+    int C = mat[0].size();
+    int col = C - 1;
+
+    // Move till we are inside the matrix
+    while (row < R && col >= 0) {
+        // If the current value is 0, move down to the next row
+        if (mat[row][col] == 0) {
+            row += 1;
+        }
+
+        else {
+            maxRow = row;
+            col -= 1;
+        }
+    }
+    return maxRow;
+}
+
+// Driver Code
+int main() {
+    vector<vector<bool>> mat = { { 0, 0, 0, 1 },
+                                 { 0, 1, 1, 1 },
+                                 { 1, 1, 1, 1 },
+                                 { 0, 0, 0, 0 } };
+
+    cout << "Index of row with maximum 1s is "
+         << rowWithMax1s(mat);
+
+    return 0;
+}
